15ccab7a0f707f970eb3341bdbd1151afdf445b5
Manishmani3 <113573478+Manishmani3@users.noreply.github.com>
2022-09-30 20:24:10 +0530

    Add files via upload

---
+++ Ifclass.java
@@ -0,0 +1,16 @@
+class Ifclass {
+    public static void main(String args[])
+    {
+        String str = "GeeksforGeeks";
+        int i = 4;
+ 
+        // if block
+        if (i == 4) {
+            i++;
+            System.out.println(str);
+        }
+ 
+        // Executed by default
+        System.out.println("i = " + i);
+    }
+}
---
+++ ifDemo.java
@@ -0,0 +1,14 @@
+class ifDemo {
+    public static void main(String[]args)
+    {
+          int i = 20;
+       
+          if (i < 21)
+              System.out.println("20 is less than 21");
+
+          System.out.println("Outside if-block");
+          // both statements will be printed 
+    }
+}
+
+
